f1281ccda36473947a9d254fd2bf822cc5cacfe5
sang joon <yyb03054@naver.com>
2021-07-15 03:00:10 +0900

    210715 03:00

--- src/main/java/board/board/controller/hellocontroller.java
+++ src/main/java/board/board/controller/hellocontroller.java
@@ -3,6 +3,8 @@ package board.board.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class hellocontroller {
@@ -10,5 +12,32 @@ public class hellocontroller {
     public String hello(Model model) {
         model.addAttribute("data", "hi!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String hellomvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
