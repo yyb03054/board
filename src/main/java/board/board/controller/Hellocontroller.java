f6a104b93082d64d6621296159969c9a8badc45b
sang joon <yyb03054@naver.com>
2021-07-15 19:04:52 +0900

    210715 03:00

--- src/main/java/board/board/controller/Hellocontroller.java
+++ src/main/java/board/board/controller/Hellocontroller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class hellocontroller {
+public class Hellocontroller {
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hi!!");
